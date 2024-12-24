96c05f4eaa4f5b836c8fa512ee4a1d4faea590d6
mvinicius175 <mauro.vinicius175@gmail.com>
2024-12-24 11:15:44 -0300

    Implementação dos gráficos de barra

--- src/data_analisys.py
+++ src/data_analisys.py
@@ -15,41 +15,41 @@ east_conference  = pd.read_csv('data/processed/east_conference.csv')
 ##########################################################################################################
 # Filtrar jogos em casa e fora
 home_games_24_25 = pistons_24_25[pistons_24_25['MATCHUP'].str.contains('@') == False]
-away_games_24_25 = pistons_24_25[pistons_24_25['MATCHUP'].str.contains('@')]
+road_games_24_25 = pistons_24_25[pistons_24_25['MATCHUP'].str.contains('@')]
 home_games_23_24 = pistons_23_24[pistons_23_24['MATCHUP'].str.contains('@') == False]
-away_games_23_24 = pistons_23_24[pistons_23_24['MATCHUP'].str.contains('@')]
+road_games_23_24 = pistons_23_24[pistons_23_24['MATCHUP'].str.contains('@')]
 
 # Contar vitórias
 total_wins_24_25 = pistons_24_25[pistons_24_25['WL'] == 'W'].shape[0]
 home_wins_24_25  = home_games_24_25[home_games_24_25['WL'] == 'W'].shape[0]
-away_wins_24_25  = away_games_24_25[away_games_24_25['WL'] == 'W'].shape[0]
+road_wins_24_25  = road_games_24_25[road_games_24_25['WL'] == 'W'].shape[0]
 total_wins_23_24 = pistons_23_24[pistons_23_24['WL'] == 'W'].shape[0]
 home_wins_23_24  = home_games_23_24[home_games_23_24['WL'] == 'W'].shape[0]
-away_wins_23_24  = away_games_23_24[away_games_23_24['WL'] == 'W'].shape[0]
+road_wins_23_24  = road_games_23_24[road_games_23_24['WL'] == 'W'].shape[0]
 
 # Contar derrotas
 total_losses_24_25 = pistons_24_25[pistons_24_25['WL'] == 'L'].shape[0]
 home_losses_24_25  = home_games_24_25[home_games_24_25['WL'] == 'L'].shape[0]
-away_losses_24_25  = away_games_24_25[away_games_24_25['WL'] == 'L'].shape[0]
+road_losses_24_25  = road_games_24_25[road_games_24_25['WL'] == 'L'].shape[0]
 total_losses_23_24 = pistons_23_24[pistons_23_24['WL'] == 'L'].shape[0]
 home_losses_23_24  = home_games_23_24[home_games_23_24['WL'] == 'L'].shape[0]
-away_losses_23_24  = away_games_23_24[away_games_23_24['WL'] == 'L'].shape[0]
+road_losses_23_24  = road_games_23_24[road_games_23_24['WL'] == 'L'].shape[0]
 
 data_pistons_24_25 = {
 	'Total Wins':   [total_wins_24_25],
 	'Home Wins':    [home_wins_24_25],
-	'Away Wins':    [away_wins_24_25],
+	'Road Wins':    [road_wins_24_25],
 	'Total Losses': [total_losses_24_25],
 	'Home Losses':  [home_losses_24_25],
-	'Away Losses':  [away_losses_24_25]
+	'Road Losses':  [road_losses_24_25]
 }
 data_pistons_23_24 = {
 	'Total Wins':   [total_wins_23_24],
 	'Home Wins':    [home_wins_23_24],
-	'Away Wins':    [away_wins_23_24],
+	'Road Wins':    [road_wins_23_24],
 	'Total Losses': [total_losses_23_24],
 	'Home Losses':  [home_losses_23_24],
-	'Away Losses':  [away_losses_23_24]
+	'Road Losses':  [road_losses_23_24]
 }
 pistons_23_24_summary = pd.DataFrame(data_pistons_23_24)
 pistons_23_24_summary.to_csv('data/exported/pistons_23_24_summary.csv', index=False)
@@ -68,8 +68,8 @@ total_3pointers_23_24   = pistons_23_24['FG3M'].sum()
 total_3pointers_24_25   = pistons_24_25['FG3M'].sum()
 total_home_losses_23_24 = home_games_23_24[home_games_23_24['WL'] == 'L'].shape[0]
 total_home_losses_24_25 = home_games_24_25[home_games_24_25['WL'] == 'L'].shape[0]
-total_away_losses_23_24 = away_games_23_24[away_games_23_24['WL'] == 'L'].shape[0]
-total_away_losses_24_25 = away_games_24_25[away_games_24_25['WL'] == 'L'].shape[0]
+total_road_losses_23_24 = road_games_23_24[road_games_23_24['WL'] == 'L'].shape[0]
+total_road_losses_24_25 = road_games_24_25[road_games_24_25['WL'] == 'L'].shape[0]
 
 data_pistons_pt1_23_24 = {
     'Total Points 23-24':       [total_points_23_24],
@@ -77,7 +77,7 @@ data_pistons_pt1_23_24 = {
     'Total Rebounds 23-24':     [total_rebounds_23_24],
     'Total 3 Points 23-24':     [total_3pointers_23_24],
     'Total Home Losses 23-24':  [total_home_losses_23_24],
-    'Total Away Losses 23-24':  [total_away_losses_23_24]
+    'Total Road Losses 23-24':  [total_road_losses_23_24]
 }
 data_pistons_pt1_24_25 = {
     'Total Points 24-25':       [total_points_24_25],
@@ -85,7 +85,7 @@ data_pistons_pt1_24_25 = {
     'Total Rebounds 24-25':     [total_rebounds_24_25],
     'Total 3 Points 24-25':     [total_3pointers_24_25],
     'Total Home Losses 24-25':  [total_home_losses_24_25],
-    'Total Away Losses 24-25':  [total_away_losses_24_25]
+    'Total Road Losses 24-25':  [total_road_losses_24_25]
 
 }
 pistons_summary_23_24_pt1 = pd.DataFrame(data_pistons_23_24)
@@ -177,11 +177,11 @@ team_name_map = {
     'UTA': 'Utah Jazz', 'WAS': 'Washington Wizards'
 }
 
-def home_or_away(matchup):
+def home_or_road(matchup):
     if "vs" in matchup:
         return "Home"
     elif "@" in matchup:
-        return "Away"
+        return "road"
     return None
 
 def get_adversary(matchup):
@@ -199,15 +199,15 @@ def get_score(pts, plus_minus):
 games_23_24 = pistons_23_24
 games_24_25 = pistons_24_25
 
-games_23_24['Home or Away'] = games_23_24['MATCHUP'].apply(home_or_away)
+games_23_24['Home or road'] = games_23_24['MATCHUP'].apply(home_or_road)
 games_23_24['Adversary'] = games_23_24['MATCHUP'].apply(get_adversary)
 games_23_24['Score'] = games_23_24.apply(lambda row: get_score(row['PTS'], row['PLUS_MINUS']), axis=1)
-games_24_25['Home or Away'] = games_24_25['MATCHUP'].apply(home_or_away)
+games_24_25['Home or road'] = games_24_25['MATCHUP'].apply(home_or_road)
 games_24_25['Adversary'] = games_24_25['MATCHUP'].apply(get_adversary)
 games_24_25['Score'] = games_24_25.apply(lambda row: get_score(row['PTS'], row['PLUS_MINUS']), axis=1)
 
-games_23_24 = games_23_24[['GAME_DATE', 'Adversary', 'WL', 'Home or Away', 'Score']]
-games_24_25 = games_24_25[['GAME_DATE', 'Adversary', 'WL', 'Home or Away', 'Score']]
+games_23_24 = games_23_24[['GAME_DATE', 'Adversary', 'WL', 'Home or road', 'Score']]
+games_24_25 = games_24_25[['GAME_DATE', 'Adversary', 'WL', 'Home or road', 'Score']]
 
 games_23_24.to_csv('data/exported/pistons_games_table_23_24.csv', index=False)
 games_24_25.to_csv('data/exported/pistons_games_table_24_25.csv', index=False)
